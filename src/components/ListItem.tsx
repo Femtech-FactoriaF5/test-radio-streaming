fc0e0ebf191f764bbb995d320c0c22ff2264952c
Raul <you@example.com>
2022-09-19 13:12:54 +0200

    test solved

---
+++ src/components/ListItem.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Radio } from './List'
+
+type listItemProps = {
+    radio:Radio;
+}
+
+export default function ListItem({radio}:listItemProps) {
+  return (
+    <li>{radio.name}</li>
+  )
+}
